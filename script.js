f41acf327ac525e4d81d2e407caff7b128db7a5c
Gabriel Alexandre  Cardoso <114122280+gabrielpoukas@users.noreply.github.com>
2026-02-23 19:08:40 -0300

    img

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const navList = document.getElementById('nav-list');
     const navLinks = navList.querySelectorAll('a');
 
-    // Função para alternar o menu (abrir/fechar)
     menuToggle.addEventListener('click', () => {
         navList.classList.toggle('active');
         const isExpanded = navList.classList.contains('active');
         menuToggle.setAttribute('aria-expanded', isExpanded);
     });
 
-    // Função para fechar o menu ao clicar em um link (para navegação em Single Page)
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
-            // Verifica se está no modo mobile (menu está aberto)
             if (navList.classList.contains('active')) {
                 navList.classList.remove('active');
                 menuToggle.setAttribute('aria-expanded', 'false');
